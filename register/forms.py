7245bb925a7d47557a5e567d183cd5a655b89f4e
kai1ban <posse.kirby@gmail.com>
2020-01-11 15:51:16 +0900

    メールアドレスを変更できるようにしました

--- register/forms.py
+++ register/forms.py
@@ -61,3 +61,19 @@ class MySetPasswordForm(SetPasswordForm):
         super().__init__(*args, **kwargs)
         for field in self.fields.values():
             field.widget.attrs['class'] = 'form-control'
+
+
+class EmailChangeForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'form-control'
+
+    def clean_email(self):  # 何度でも仮登録を以下略
+        email = self.cleaned_data['email']
+        User.objects.filter(email=email, is_active=False).delete()
+        return email
